9578d1975dd167ac64d702a0a93a61668ed68df7
James Wedgwood <jtbwedgwood@gmail.com>
2025-09-14 11:25:08 -0400

    Part 1.1.3 working

--- chapter1_transformer_interp/exercises/part1.py
+++ chapter1_transformer_interp/exercises/part1.py
@@ -283,7 +283,6 @@ class DemoTransformer(nn.Module):
             resid = block(resid)
         resid = self.ln_final(resid)
         return self.unembed(resid)
-            
 
 # %%
 
@@ -360,3 +359,154 @@ for i in range(100):
     test_string += reference_gpt2.tokenizer.decode(demo_logits[-1, -1].argmax())
 
 print(test_string)
+
+# %%
+
+def get_log_probs(
+    logits: Float[Tensor, "batch posn d_vocab"], tokens: Int[Tensor, "batch posn"]
+) -> Float[Tensor, "batch posn-1"]:
+    log_probs = logits.log_softmax(dim=-1)
+    # Get logprobs the first seq_len-1 predictions (so we can compare them with the actual next tokens)
+    log_probs_for_tokens = (
+        log_probs[:, :-1].gather(dim=-1, index=tokens[:, 1:].unsqueeze(-1)).squeeze(-1)
+    )
+
+    return log_probs_for_tokens
+
+model_cfg = Config(
+    debug=False,
+    d_model=32,
+    n_heads=16,
+    d_head=2,
+    d_mlp=32 * 4,
+    n_layers=4,
+    n_ctx=128,
+    d_vocab=reference_gpt2.cfg.d_vocab,
+)
+model = DemoTransformer(model_cfg)
+
+@dataclass
+class TransformerTrainingArgs:
+    batch_size: int = 32
+    epochs: int = 10
+    max_steps_per_epoch: int = 500
+    lr: float = 1e-3
+    weight_decay: float = 1e-2
+    wandb_project: str | None = "day1-demotransformer"
+    wandb_name: str | None = None
+
+
+args = TransformerTrainingArgs()
+
+dataset = datasets.load_dataset("roneneldan/TinyStories", split="train")
+
+tokenized_dataset = tokenize_and_concatenate(
+    dataset,
+    reference_gpt2.tokenizer,
+    streaming=False,
+    max_length=model.cfg.n_ctx,
+    column_name="text",
+    add_bos_token=True,
+    num_proc=4,
+)
+
+dataset_dict = tokenized_dataset.train_test_split(test_size=1000)
+
+
+class TransformerTrainer:
+    def __init__(self, args: TransformerTrainingArgs, model: DemoTransformer):
+        super().__init__()
+        self.model = model
+        self.args = args
+        self.sampler = solutions.TransformerSampler(self.model, reference_gpt2.tokenizer)
+        self.optimizer = t.optim.AdamW(
+            self.model.parameters(), lr=args.lr, weight_decay=args.weight_decay
+        )
+        self.step = 0
+
+        self.train_loader = DataLoader(
+            dataset_dict["train"],
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=4,
+            pin_memory=True,
+        )
+        self.test_loader = DataLoader(
+            dataset_dict["test"],
+            batch_size=args.batch_size,
+            shuffle=False,
+            num_workers=4,
+            pin_memory=True,
+        )
+
+    def training_step(self, batch: dict[str, Int[Tensor, "batch seq"]]) -> Float[Tensor, ""]:
+        """
+        Calculates the loss on the tokens in the batch, performs a gradient update step, and logs the loss.
+
+        Remember that `batch` is a dictionary with the single key 'tokens'.
+        """
+        tokens = batch['tokens'].to(device)
+        logits = self.model(tokens)
+        pred_log_probs = get_log_probs(logits, tokens)
+        loss = -pred_log_probs.mean()
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+        self.step += 1
+        wandb.log({"train_loss": loss}, step=self.step)
+        return loss
+
+    @t.inference_mode()
+    def evaluate(self) -> float:
+        """
+        Evaluate the model on the test set and return the accuracy.
+        """
+        self.model.eval()
+        total_correct, total_samples = 0, 0
+
+        for batch in tqdm(self.test_loader):
+            tokens = batch['tokens'].to(device)
+            logits = self.model(tokens)[:, :-1]
+            pred = logits.argmax(dim=-1)
+            total_correct += (pred == tokens[:, 1:]).sum().item()
+            total_samples += tokens.size(0) * (tokens.size(1) - 1)
+
+        accuracy = total_correct / total_samples
+        wandb.log({"accuracy": accuracy}, step=self.step)
+
+        self.model.train()
+        return accuracy
+
+    def train(self):
+        """
+        Trains the model, for `self.args.epochs` epochs. Also handles wandb initialisation, and early stopping
+        for each epoch at `self.args.max_steps_per_epoch` steps.
+        """
+        wandb.login(key=open('/home/jwedgwoo/ARENA_3.0/api_key_wandb').read())
+        wandb.init(project=self.args.wandb_project, name=self.args.wandb_name, config=self.args)
+        accuracy = np.nan
+
+        progress_bar = tqdm(total=self.args.max_steps_per_epoch * self.args.epochs)
+
+        for epoch in range(self.args.epochs):
+            for i, batch in enumerate(self.train_loader):
+                loss = self.training_step(batch)
+                progress_bar.update()
+                progress_bar.set_description(
+                    f"Epoch {epoch + 1}, loss: {loss:.3f}, accuracy: {accuracy:.3f}"
+                )
+                if i >= self.args.max_steps_per_epoch:
+                    break
+
+            accuracy = self.evaluate()
+            sample_text = self.sampler.sample("Once upon a time", max_tokens_generated=50)
+            print(sample_text)
+
+        wandb.finish()
+
+
+# See the full run here: https://api.wandb.ai/links/dquarel/nrxuwnv7
+model = DemoTransformer(model_cfg).to(device)
+args = TransformerTrainingArgs()
+trainer = TransformerTrainer(args, model)
+trainer.train()
